511548d43c655842967d26fb081f3d0629123eb0
jeffubayi <jeffubayi@gmail.com>
2023-05-10 13:08:34 +0300

    add pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,16 @@
-import { Auth } from '@supabase/auth-ui-react'
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Link from '@mui/material/Link';
 import { useEffect } from "react";
-import { supabase } from "../utility/supabaseClient";
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { useRouter } from "next/router";
-import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import * as React from 'react';
+import { Avatar, Box, Button, CssBaseline, Grid, Link, Paper, TextField, Typography, Divider } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import GoogleIcon from '@mui/icons-material/Google';
 
 function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="jeffubayi.vercel.app">
+      <Link color="inherit" href="https://mui.com/">
         @ubeezy
       </Link>{' '}
       {new Date().getFullYear()}
@@ -24,35 +19,118 @@ function Copyright(props: any) {
   );
 }
 
-export default function Home() {
+
+export default function SignInSide() {
   const router = useRouter();
   const session = useSession()
   const supabase = useSupabaseClient()
 
-  useEffect(() => {
-    if (session) {
-      if (router.pathname === "/") {
-        router.push("/flights");
-      }
-    }
-  }, [router, session]);
+  // useEffect(() => {
+  //   if (session) {
+  //     if (router.pathname === "/") {
+  //       router.push("/flights");
+  //     }
+  //   }
+  // }, [router, session]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+    router.push("/Home");
+  };
+
 
   return (
-    <Container component="main" maxWidth="md">
+    <Grid container component="main" sx={{ height: '100vh' }}>
       <CssBaseline />
-      <Box
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={8}
         sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          backgroundImage: 'url(https://lh3.googleusercontent.com/Vrmz8X7tAk7W7ghz6oQ9Ik2Z95UtS6LPPR1mUYWjtTNcdMDJvelHxTgfVeWj5hRMa1iVriE0vTp3HHKNfRC8CzObwSLwGS14WPw87g=w1400-v0)',
+          backgroundRepeat: 'repeat',
+          backgroundColor: (t) =>
+            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
         }}
-      >
-        <Auth
-          supabaseClient={supabase} appearance={{ theme: ThemeSupa }} magicLink providers={['google']}
-          redirectTo="/" theme="light" />
-      </Box>
-      <Copyright sx={{ mt: 8, mb: 4 }} />
-    </Container>
+      />
+      <Grid item xs={12} sm={8} md={4} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              size="small"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              size="small"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              color="primary"
+              fullWidth
+              size="small"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Divider>Or sign in via</Divider>
+            <Button
+              type="button"
+              startIcon={<GoogleIcon />}
+              color="primary"
+              fullWidth
+              size="small"
+              variant="outlined"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Google
+            </Button>
+                <Typography variant="body2" color="text.secondary" align="center">
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Typography>
+            <Copyright sx={{ mt: 5 }} />
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
