f9676d418eb79344ce2ffe009087fee8b50b8d0c
jeffubayi <jeffubayi@gmail.com>
2023-05-10 18:53:23 +0300

    add utils

---
+++ utility/enums.ts
@@ -0,0 +1,39 @@
+export const tiers = [
+    {
+        title: 'Components',
+        subheader: 'Material UI',
+        price: '/about',
+        description: [
+            'UI components',
+            'Themes',
+            'Layout',
+        ],
+        buttonText: 'View',
+        buttonVariant: 'outlined',
+    },
+    {
+        title: 'Authentication',
+        subheader: 'Supabase',
+        price: '/signin',
+        description: [
+
+            'Session Management',
+            'User Sign Up/In',
+            'Social Login',
+        ],
+        buttonText: 'Sign Up',
+        buttonVariant: 'contained',
+    },
+    {
+        title: 'React',
+        subheader: 'Nextjs',
+        price: '/flights',
+        description: [
+            'State Management',
+            'Data Fetching(SSR)',
+            'Error Handling',
+        ],
+        buttonText: 'View',
+        buttonVariant: 'outlined',
+    },
+];
